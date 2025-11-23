f48faa986084c62c2a6db8f4d69dac0008f89f2f
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-11-23 18:24:46 +0600

    Ts simple types module

---
+++ TS-Simple-Types/index.js
@@ -0,0 +1,7 @@
+var Fullname = "Kabirul";
+var sentence = "Hello, my name is ".concat(Fullname);
+console.log(sentence);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 6));
---
+++ TS-Simple-Types/index.ts
@@ -0,0 +1,10 @@
+let Fullname:string = "Kabirul";
+let sentence:string = `Hello, my name is ${Fullname}`;
+
+console.log(sentence);
+
+function add(a:number, b:number):number{
+    return a + b;
+}
+
+console.log(add(5,6));
